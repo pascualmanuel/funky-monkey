fc6e0815859e91efbb813f4031b801cf664d0f7c
manuel <manuelnpascual@gmail.com>
2025-09-29 18:51:36 +0200

    Form hasta < 768px

--- src/app/retreats/page.js
+++ src/app/retreats/page.js
@@ -1,3 +1,4 @@
+"use client";
 import Layout from "@/components/Layout";
 import RetreatsImage from "@/assets/retreats/retreats.webp";
 import Retreats4 from "@/assets/retreats/retreats-4.webp";
@@ -11,8 +12,48 @@ import BeachIcon from "@/assets/retreats/beach-icon.svg";
 import BedIcon from "@/assets/retreats/bed-icon.svg";
 import CarIcon from "@/assets/retreats/car-icon.svg";
 import FoodIcon from "@/assets/retreats/food-icon.svg";
+import WhatsApp from "@/assets/santa-teresa/whatsapp-icon.svg";
+import { useState } from "react";
 
 export default function Retreats() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    kindOfRetreat: "",
+    participants: "",
+  });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    // Simulate form submission
+    setTimeout(() => {
+      setIsSubmitting(false);
+      setIsSuccess(true);
+
+      // Reset success state after 3 seconds
+      setTimeout(() => {
+        setIsSuccess(false);
+        setFormData({
+          name: "",
+          email: "",
+          kindOfRetreat: "",
+          participants: "",
+        });
+      }, 3000);
+    }, 1000);
+  };
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <Layout title="Wellness Retreats">
       <div
@@ -205,6 +246,165 @@ export default function Retreats() {
               <p className="body1 text-darkGrey">
                 Assistance with planning activities, excursions, meals and more.
               </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mx-4 sm:mx-8 lg:mx-[70px]">
+        <div className=" overflow-hidden bg-blue rounded-[16px]  my-[100px]">
+          <div className="flex flex-col lg:flex-row items-center py-20 px-[64px] justify-between">
+            <div className="w-full lg:w-[40%] ">
+              <h4 className="myH2 text-white">
+                Let's plan your retreat or event in Santa Teresa
+              </h4>
+              <p className="body1 text-grey2 mt-4 mb-[50px]">
+                Let us help you bring your vision to life in one of Costa Rica's
+                most magical locations.
+              </p>
+              <div className="bgreen-gradient2 text-white text-center py-4 px-6 rounded-[58px] max-w-[200px] flex items-center gap-2 justify-center font-bold">
+                <img src={WhatsApp.src} alt="WhatsApp" /> WhatsApp us
+              </div>
+            </div>
+            <div className="w-full lg:w-[60%]  ">
+              <form onSubmit={handleSubmit} className="max-w-[520px] ml-auto ">
+                {/* Name and Email Row */}
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                  <div>
+                    <label
+                      htmlFor="name"
+                      className="block text-white text-base font-bold mb-2"
+                    >
+                      Name*
+                    </label>
+                    <input
+                      type="text"
+                      id="name"
+                      name="name"
+                      value={formData.name}
+                      onChange={handleChange}
+                      placeholder="Name..."
+                      required
+                      className="w-full px-4 py-3 rounded-lg bg-[#FFFFFF1A]  text-white placeholder-gray-300 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent transition-all duration-200"
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block text-white text-base font-bold mb-2"
+                    >
+                      Email*
+                    </label>
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      value={formData.email}
+                      onChange={handleChange}
+                      placeholder="Email..."
+                      required
+                      className="w-full px-4 py-3 rounded-lg bg-[#FFFFFF1A]  text-white placeholder-gray-300 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent transition-all duration-200"
+                    />
+                  </div>
+                </div>
+
+                {/* Kind of retreat */}
+                <div>
+                  <label
+                    htmlFor="kindOfRetreat"
+                    className="block text-white text-base font-bold mb-2 mt-4"
+                  >
+                    Kind of retreat:*
+                  </label>
+                  <textarea
+                    id="kindOfRetreat"
+                    name="kindOfRetreat"
+                    value={formData.kindOfRetreat}
+                    onChange={handleChange}
+                    placeholder="Write your message..."
+                    required
+                    rows={2}
+                    className="w-full px-4 py-3 rounded-lg bg-[#FFFFFF1A]  text-white placeholder-gray-300 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent transition-all duration-200 resize-none"
+                  />
+                </div>
+
+                {/* Amount of participants */}
+                <div>
+                  <label
+                    htmlFor="participants"
+                    className="block text-white text-base font-bold mb-2 mt-4"
+                  >
+                    Amount of participants:*
+                  </label>
+                  <textarea
+                    id="participants"
+                    name="participants"
+                    value={formData.participants}
+                    onChange={handleChange}
+                    placeholder="Write your message..."
+                    required
+                    rows={1}
+                    className="w-full px-4 py-3 rounded-lg bg-[#FFFFFF1A]  text-white placeholder-gray-300 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent transition-all duration-200 resize-none"
+                  />
+                </div>
+
+                {/* Submit Button */}
+                <button
+                  type="submit"
+                  disabled={isSubmitting}
+                  className={`w-full py-3 px-6 rounded-lg font-medium text-white transition-all duration-300 transform mt-6 ${
+                    isSuccess
+                      ? "bg-green scale-101"
+                      : isSubmitting
+                      ? "bg-green opacity-75 cursor-not-allowed"
+                      : "bg-green cursor-pointer hover:bg-[#176221] hover:scale-101 active:scale-99"
+                  }`}
+                >
+                  {isSuccess ? (
+                    <span className="flex items-center justify-center">
+                      <svg
+                        className="w-5 h-5 mr-2"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth={2}
+                          d="M5 13l4 4L19 7"
+                        />
+                      </svg>
+                      Message Sent!
+                    </span>
+                  ) : isSubmitting ? (
+                    <span className="flex items-center justify-center">
+                      <svg
+                        className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                      >
+                        <circle
+                          className="opacity-25"
+                          cx="12"
+                          cy="12"
+                          r="10"
+                          stroke="currentColor"
+                          strokeWidth="4"
+                        ></circle>
+                        <path
+                          className="opacity-75"
+                          fill="currentColor"
+                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                        ></path>
+                      </svg>
+                      Sending...
+                    </span>
+                  ) : (
+                    "Send"
+                  )}
+                </button>
+              </form>
             </div>
           </div>
         </div>
